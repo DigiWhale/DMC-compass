f9c4fcfaba38c8cdd38f4cd30ac0e9151e2fca44
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-08 17:29:56 -0400

    1

--- compass.py
+++ compass.py
@@ -3,4 +3,6 @@ import serial
 ser = serial.Serial(port='/dev/ttyUSB1', baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=1)
 
 while True:
-    print(hex(int(ser.read().encode('hex'), 16)))
+    in_bin = ser.read()
+    in_hex = hex(int.from_bytes(in_bin,byteorder='little'))
+    print(in_hex)
