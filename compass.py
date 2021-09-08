5e2962a9fb9f33c7a5dcbc395523b45e9a52e6d9
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-08 19:54:56 -0400

    1

--- compass.py
+++ compass.py
@@ -1,11 +1,11 @@
 import serial
 import redis
 
-ser = serial.Serial(port='/dev/ttyUSB1', baudrate=115200, bytesize=8, parity='N', stopbits=1, timeout=5)
+ser = serial.Serial(port='/dev/ttyUSB1', baudrate=19200, bytesize=8, parity='N', stopbits=1, timeout=5)
 print("Connected to: " + ser.portstr)
 ser.read()
 ser.close()
-ser = serial.Serial(port='/dev/ttyUSB1', baudrate=115200, bytesize=8, parity='N', stopbits=1, timeout=5)
+ser = serial.Serial(port='/dev/ttyUSB1', baudrate=19200, bytesize=8, parity='N', stopbits=1, timeout=5)
 r = redis.Redis(host='192.168.1.4', port=6379, db=0, password='Redis2019!')
 ser.flushInput()
 while True:
