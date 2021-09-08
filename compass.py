297fb6841b2ad25011825f2307bd8683ad62dbf9
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-08 17:36:12 -0400

    1

--- compass.py
+++ compass.py
@@ -1,7 +1,7 @@
 import serial
 
 ser = serial.Serial(port='/dev/ttyUSB1', baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=1)
-
+ser.flushInput()
 while True:
     in_bin = ser.readline()
     print(in_bin[0:len(in_bin)-2].decode("utf-8"))
