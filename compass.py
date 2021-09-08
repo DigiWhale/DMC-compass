1965edc7aee3add42a268a5315f7d3bcc8115144
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-08 19:08:30 -0400

    1

--- compass.py
+++ compass.py
@@ -1,6 +1,6 @@
 import serial
 
-ser = serial.Serial(port='/dev/ttyUSB1', baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=1)
+ser = serial.Serial(port='/dev/ttyUSB1', baudrate=19200, bytesize=8, parity='N', stopbits=1, timeout=5)
 
 while True:
     in_bin = ser.readline()
