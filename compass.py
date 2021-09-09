ff8aa7391433cfcb1dab20958310a47c22d7cb87
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-08 20:15:08 -0400

    1

--- compass.py
+++ compass.py
@@ -9,7 +9,7 @@ print("Connected to: " + ser.portstr)
 r = redis.Redis(host='192.168.1.4', port=6379, db=0, password='Redis2019!')
 # ser.flushInput()
 while True:
-    in_bin = ser.read()
+    in_bin = ser.readline()
     print(in_bin)
     # if in_bin.strip().find(b'$HCHDG')==0:
     #     data_list = in_bin.strip().decode('utf-8').split(',')
